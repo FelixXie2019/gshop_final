6bfd7f70165d6cd16f1ac0c92abd703663fbb362
zifeng <xie504519449@gmail.com>
2021-02-20 15:08:06 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import 'lib-flexible'
+import {Button} from 'mint-ui'
+
+import App from './App.vue'
+import router from './router'
+import Header from './components/Header/Header.vue'
+import Star from './components/Star/Star.vue'
+import store from './vuex/store'
+import './validate'
+import * as API from '@/api'
+import i18n from './i18n'
+import './mock/mock-server'
+
+// 将API对象挂载到Vue的原型对象上
+Vue.prototype.$API = API
+
+// 注册全局组件
+Vue.component('Header', Header)
+Vue.component('Star', Star)
+Vue.component(Button.name, Button)  // mt-button
+
+new Vue({
+  // components: {
+  //   App
+  // },
+  // template: '<App/>'
+  render: h => h(App),
+
+  // 所有组件都能看到 $router和$route  <router-link> 和 <router-view/>
+  router,
+
+  i18n,
+
+  // 所有组件都能看到: $store
+  store
+}).$mount('#app')
---
+++ src/router/routes.js
@@ -0,0 +1,75 @@
+/* 
+所有路由配置的数组
+*/
+
+import MSite from '@/pages/MSite/MSite'
+import Search from '@/pages/Search/Search'
+import Order from '@/pages/Order/Order'
+import Profile from '@/pages/Profile/Profile'
+import Login from '@/pages/Login/Login'
+import Shop from '@/pages/Shop/Shop'
+import Goods from '@/pages/Shop/Goods'
+import Ratings from '@/pages/Shop/Ratings'
+import Info from '@/pages/Shop/Info'
+
+export default [
+  {
+    path: '/msite',
+    component: MSite,
+    meta: {
+      isShowFooter: true
+    }
+  },
+  {
+    path: '/search',
+    component: Search,
+    meta: {
+      isShowFooter: true
+    }
+  },
+  {
+    path: '/order',
+    component: Order,
+    meta: {
+      isShowFooter: true
+    }
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: {
+      isShowFooter: true
+    }
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/shop',
+    component: Shop,
+    children: [
+      {
+        path: '/shop/goods',
+        component: Goods
+      },
+      {
+        path: 'ratings',
+        component: Ratings
+      },
+      {
+        path: '/shop/info',
+        component: Info
+      },
+      {
+        path: '',
+        redirect: '/shop/goods'
+      }
+    ]
+  },
+
+  {
+    path: '/',
+    redirect: '/msite'
+  }
+]
